58dee0370713015d8526a69f86b596e0881d0ab3
DouglasGo8 <abraham.dias.douglas@gmail.com>
2023-07-19 01:56:39 -0300

    First Commit

--- shared-domain/src/main/java/com/inhouse/mock/modules/pattern/shared/domain/valueobject/CloudEvent.java
+++ shared-domain/src/main/java/com/inhouse/mock/modules/pattern/shared/domain/valueobject/CloudEvent.java
@@ -9,7 +9,7 @@ import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Getter
-@Builder
+@Builder // Fix with inner builder
 public class CloudEvent<E extends EventData> extends BaseId<UUID> {
 
   final String type;
